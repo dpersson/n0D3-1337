cd58add6cc97accbb6adada814aececf696fcc78
Daniel Persson <dpersson@gmail.com>
2010-09-25 17:24:43 +0200

    Remove unneccesary requires

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 require.paths.unshift(__dirname + '/lib');
 require.paths.unshift(__dirname);
-require.paths.unshift(__dirname + '/deps/express/lib');
+require.paths.unshift(__dirname + '/deps/express/lib')
 
 var sys   = require('sys');
 var fs    = require('fs');
 var mongo = require('deps/node-mongodb-native/lib/mongodb');
 
-require('express');
-require('express/plugins');
+require('deps/express');
 
 
